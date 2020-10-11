65f7d48722f85a3dc00fb94a40d102df3e72060c
devdave <devdave@ominian.net>
2020-10-10 19:28:40 -0600

    remove flotsom from a past project

--- views/base.py
+++ views/base.py
@@ -2,7 +2,6 @@
 from flask import render_template
 from logging import getLogger
 from .. import app
-from ..lib.forms import Donor, CustomerRegister
 
 log = getLogger(__name__)
 
@@ -10,7 +9,4 @@ log = getLogger(__name__)
 @app.route("/", methods=["GET"])
 def index():
 
-    donor_form = Donor()
-    customer_form = CustomerRegister()
-
-    return render_template("home.html", donor = donor_form, customer=customer_form)
+    return render_template("page_basic.html")
